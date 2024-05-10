1bfea7d499f4078f3897e8737ad1894b38cb0f38
Sinth <sinthmite@gmail.com>
2024-05-10 15:24:34 -0400

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import LogIn from './LogIn/LogIn.jsx';
 import Header from './Header/Header.jsx';
 import UserDefined from './UserDefined/UserDefined.jsx';
+
 function App({ Firebase }) {
-  const [logIn, setLogIn] = useState(false);
+  const [logIn, setLogIn] = useState(true);
 
   return (
     <Router>
       <Routes>
-        {!logIn &&<Route path="/" exact element={<LogIn Firebase={Firebase} setLogIn={setLogIn} />} />}
-        {logIn &&<Route path='/Home' element={<Header Firebase={Firebase} setLogIn={setLogIn} />} />}
+        {!logIn && (
+          <>
+            <Route path="/WalletWise" element={<LogIn Firebase={Firebase} setLogIn={setLogIn} />} />
+            <Route path="/WalletWise/Home" element={<Navigate to="/WalletWise" />} />
+          </>
+        )}
+        {logIn && (
+          <>
+            <Route path="/WalletWise" element={<Header Firebase={Firebase} setLogIn={setLogIn} />} />
+            <Route path="/WalletWise/Home" element={<Header Firebase={Firebase} setLogIn={setLogIn} />} />
+          </>
+        )}
       </Routes>
-      {/* Define more routes here if needed */}
     </Router>
   );
 }
